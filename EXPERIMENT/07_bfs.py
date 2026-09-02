dae46c0c9a412ad3057554e43ccb41a7a44e112d
Asha Sarthiwika <ashasugavelamarthi@gmail.com>
2026-09-02 11:24:46 +0530

    Add BFS algorithm implementation in 07_bfs.py
    Implement breadth-first search (BFS) algorithm to traverse a graph.

---
+++ EXPERIMENT/07_bfs.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+graph = {
+    "A": ["B", "C"],
+    "B": ["D", "E"],
+    "C": ["F"],
+    "D": [],
+    "E": ["F"],
+    "F": []
+}
+
+def bfs(start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+
+            for neighbour in graph[node]:
+                queue.append(neighbour)
+
+
+print("BFS Traversal:")
+bfs("A")
